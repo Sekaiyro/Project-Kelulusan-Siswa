c95e8029812b4e7fd03749832b40144bedde7311
Adrian Pramana Wijaya <106787436+Adrian-Pramana-Wijaya@users.noreply.github.com>
2022-06-04 10:13:21 +0700

    Add files via upload

---
+++ Main1/src/main1/Main1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        interfaceB obj = new interfaceB();
+        obj.perkenalan();
+        obj.biodata("Adrian Pramana Wijaya XRPL2", "LULUS");
+        obj.sekolah();
+        obj.salam();
+    }
+    
+}
---
+++ Main1/src/main1/interfaceB.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class interfaceB implements interfaceA, interfaceAA {
+
+    public void perkenalan() {
+        System.out.println("Sistem Kelulusan Siswa SMK Telkom Malang");
+    }
+
+    public void biodata(String nama, String kelas) {
+        System.out.println("Nama Siswa "+nama+" dan Ananda Dinyatakan "+kelas);
+    }
+
+    public void sekolah() {
+        System.out.println("Selamat Dan Sukses Kepada Ananda Adrian Pramana Wijaya");
+    }
+
+    
+    }
+    
+
